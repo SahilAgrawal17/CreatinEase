4ba741666070e7124e98274d137341da02b7e450
Sahil Agrawal <122197803+SahilAgrawal17@users.noreply.github.com>
2024-03-05 00:11:13 +0530

    Average RGB Values of all pixels in an image

---
+++ rgb.py
@@ -0,0 +1,37 @@
+# Import Module
+from PIL import Image
+
+def most_common_used_color(img):
+    # Get width and height of Image
+    width, height = img.size
+
+    # Initialize Variable
+    r_total = 0
+    g_total = 0
+    b_total = 0
+
+    count = 0
+
+    # Iterate through each pixel
+    for x in range(0, width):
+        for y in range(0, height):
+            # r,g,b value of pixel
+            r, g, b = img.getpixel((x, y))
+
+            r_total += r
+            g_total += g
+            b_total += b
+            count += 1
+
+    return (r_total/count, g_total/count, b_total/count)
+
+# Read Image
+img = Image.open("SoumyaTest1.jpg")
+
+# Convert Image into RGB
+img = img.convert('RGB')
+
+# call function
+common_color = most_common_used_color(img)
+
+print(common_color)
